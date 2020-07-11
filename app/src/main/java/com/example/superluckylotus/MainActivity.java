e25da11d3f7565a2435477546560803c9c1c88d2
mokuto0817 <mokuto0817@icloud.com>
2020-07-11 23:55:04 +0800

    添加页面转换

--- app/src/main/java/com/example/superluckylotus/MainActivity.java
+++ app/src/main/java/com/example/superluckylotus/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
@@ -23,138 +24,73 @@ public class MainActivity extends AppCompatActivity  {
     private NearFragment nearFragment;
     private NoticeFragment noticeFragment;
 
-    private static final String[] FRAGMENT_TAG = {"earth_tab","near_tab", "notice_tab","me_tab"};
-
-    private final int show_tab_earth = 0;//附近
-    private final int show_tab_near = 1;//广场
-    private final int show_tab_notice = 2;//消息
-    private final int show_tab_me = 3;//我的
-    private int mrIndex = show_tab_earth;//默认选中地图
-
-    private static final String PRV_SELINDEX = "PREV_SELINDEX";
-    private int index = -100;// 记录当前的选项
     private FragmentManager fm;
+
+    private Button earth_Btn;
+    private Button near_Btn;
+    private Button notice_Btn;
+    private Button me_Btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //没有标题栏
         setContentView(R.layout.activity_main);
-        //防止app闪退后fragment重叠
-        if (savedInstanceState != null) {
-            //读取上一次界面Save的时候tab选中的状态
-            mrIndex = savedInstanceState.getInt(PRV_SELINDEX, mrIndex);
-            earthFragment = (EarthFragment) fm.findFragmentByTag(FRAGMENT_TAG[0]);
-            nearFragment = (NearFragment) fm.findFragmentByTag(FRAGMENT_TAG[1]);
-            noticeFragment = (NoticeFragment) fm.findFragmentByTag(FRAGMENT_TAG[2]);
-            meFragment = (MeFragment) fm.findFragmentByTag(FRAGMENT_TAG[3]);
 
-            //初始化
-            initView();
-        }
+        //实例化EarthFragment
+        earthFragment = new EarthFragment();
+        //把EarthFragment添加到Avtivity中
+        getFragmentManager().beginTransaction().add(R.id.fragment_container,earthFragment).commitAllowingStateLoss();
 
-    }
-
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt(PRV_SELINDEX, mrIndex);
-        super.onSaveInstanceState(outState);
-    }
-
-    private void initView() {
-        //获得RadioGroup控件
-        mTabRadioGroup=findViewById(R.id.tabs_rg);
-        setTabSelection(show_tab_earth);//初始页面为广场
-        mTabRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        earth_Btn = (Button)findViewById(R.id.earth_tab);
+        earth_Btn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                switch (checkedId){
-                    case R.id.earth_tab:
-                        setTabSelection(show_tab_earth);
-                        break;
-                    case R.id.near_tab:
-                        setTabSelection(show_tab_near);
-                        break;
-                    case R.id.notice_tab:
-                        setTabSelection(show_tab_notice);
-                        break;
-                    case R.id.me_tab:
-                        setTabSelection(show_tab_me);
-                        break;
-                    default:
-                        break;
+            public void onClick(View v){
+                if(earthFragment == null)
+                {
+                    earthFragment = new EarthFragment();
                 }
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,earthFragment).commitAllowingStateLoss();
             }
         });
-    }
 
-//根据传入的index参数来设置选中tab页
-    private void setTabSelection(int id){
-        if(id==index){
-            return;
-        }
-        index=id;
-        FragmentTransaction transaction=fm.beginTransaction();
-        hideFragments(transaction);
-        switch(index){
-            case 0:
-                mTabRadioGroup.check(R.id.earth_tab);
-                if(earthFragment==null){
-                    earthFragment=new EarthFragment();
-                    transaction.add(R.id.fragment_container,earthFragment,FRAGMENT_TAG[index]);
-                }else{
-                    transaction.show(earthFragment);
+        near_Btn = (Button)findViewById(R.id.near_tab);
+        near_Btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(nearFragment == null)
+                {
+                    nearFragment = new NearFragment();
                 }
-                transaction.commit();
-                break;
-            case 1:
-                mTabRadioGroup.check(R.id.near_tab);
-                if(nearFragment==null){
-                    nearFragment=new NearFragment();
-                    transaction.add(R.id.fragment_container,nearFragment,FRAGMENT_TAG[index]);
-                }else{
-                    transaction.show(nearFragment);
-                }
-                transaction.commit();
-                break;
-            case 2:
-                mTabRadioGroup.check(R.id.notice_tab);
-                if(noticeFragment==null){
-                    noticeFragment=new NoticeFragment();
-                    transaction.add(R.id.fragment_container,noticeFragment,FRAGMENT_TAG[index]);
-                }else{
-                    transaction.show(noticeFragment);
-                }
-                transaction.commit();
-                break;
-            case 3:
-                mTabRadioGroup.check(R.id.me_tab);
-                if(meFragment==null){
-                    meFragment=new MeFragment();
-                    transaction.add(R.id.fragment_container,meFragment,FRAGMENT_TAG[index]);
-                }else{
-                    transaction.show(meFragment);
-                }
-                transaction.commit();
-                break;
-        }
-    }
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,nearFragment).commitAllowingStateLoss();
+            }
+        });
 
-    private void hideFragments(FragmentTransaction transaction) {
-        if (earthFragment != null) {
-            transaction.hide(earthFragment);
-        }
-        if (nearFragment != null) {
-            transaction.hide(nearFragment);
-        }
-        if (noticeFragment != null) {
-            transaction.hide(noticeFragment);
-        }
-        if (meFragment != null) {
-            transaction.hide(noticeFragment);
-        }
+        notice_Btn = (Button)findViewById(R.id.notice_tab);
+        notice_Btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(noticeFragment == null)
+                {
+                    noticeFragment = new NoticeFragment();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,noticeFragment).commitAllowingStateLoss();
+            }
+        });
+
+        me_Btn = (Button)findViewById(R.id.me_tab);
+        me_Btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(meFragment == null)
+                {
+                    meFragment = new MeFragment();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,meFragment).commitAllowingStateLoss();
+            }
+        });
+
 
     }
-
-
 
 }
