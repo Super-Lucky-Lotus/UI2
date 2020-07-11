d53d2fbe506663d42a044b299aca77626b1df818
蛋蛋 <for.s@qq.com>
2020-07-11 14:09:53 +0800

    基本界面

--- app/src/main/java/com/example/superluckylotus/MainActivity.java
+++ app/src/main/java/com/example/superluckylotus/MainActivity.java
@@ -1,139 +1,124 @@
 package com.example.superluckylotus;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.app.Activity;
 import android.app.FragmentManager;
+
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    private EarthFragment earthTabPage=new EarthFragment();
-    private NoticeFragment noticeTabPage=new NoticeFragment();
-    private MeFragment meTabPage=new MeFragment();
-    private NearFragment nearTabPage=new NearFragment();
+    private RadioGroup mTabRadioGroup;
+    private MeFragment meFragment;
+    private EarthFragment earthFragment;
+    private NearFragment nearFragment;
+    private NoticeFragment noticeFragment;
 
     private FragmentManager fm;
 
-    private LinearLayout mEarthTabButton;
-    private LinearLayout mNoticeTabButton;
-    private LinearLayout mMyTabButton;
-    private LinearLayout mNearTabButton;
-
-    private ImageButton mEarthImageButton;
-    private ImageButton mNoticeImageButton;
-    private ImageButton mMyImageButton;
-    private ImageButton mNearImageButton;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //没有标题栏
         setContentView(R.layout.activity_main);
         initView();
-        initFragment();
-        selectFragment(0);
-        initEvent();
-    }
-    private void initEvent() {
-        mMyTabButton.setOnClickListener(this);
-        mEarthTabButton.setOnClickListener(this);
-        mNoticeTabButton.setOnClickListener(this);
-        mNearTabButton.setOnClickListener(this);
     }
 
     private void initView() {
-        mEarthImageButton=(ImageButton)findViewById(R.id.id_imgButton_earth);
-        mNoticeImageButton=(ImageButton)findViewById(R.id.id_imgButton_notice);
-        mMyImageButton=(ImageButton)findViewById(R.id.id_imgButton_my);
-        mNearImageButton=(ImageButton)findViewById(R.id.id_imgButton_near);
-
-        mEarthTabButton=(LinearLayout)findViewById(R.id.id_tabButton_earth);
-        mNoticeTabButton=(LinearLayout)findViewById(R.id.id_tabButton_notice);
-        mMyTabButton = (LinearLayout) findViewById(R.id.id_tabButton_my);
-        mNearTabButton=(LinearLayout)findViewById(R.id.id_tabButton_near);
-
+        //获得RadioGroup控件
+        mTabRadioGroup=findViewById(R.id.tabs_rg);
+        setTabSelection(0);//初始页面为广场
+        mTabRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId){
+                    case R.id.earth_tab:
+                        setTabSelection(0);
+                        break;
+                    case R.id.near_tab:
+                        setTabSelection(1);
+                        break;
+                    case R.id.notice_tab:
+                        setTabSelection(2);
+                        break;
+                    case R.id.my_tab:
+                        setTabSelection(3);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
-    private void initFragment() {
-        fm=getFragmentManager();
+//根据传入的index参数来设置选中tab页
+    private void setTabSelection(int id){
         FragmentTransaction transaction=fm.beginTransaction();
-        transaction.add(R.id.id_content,earthTabPage).addToBackStack(null).commitAllowingStateLoss();
-        transaction.add(R.id.id_content,nearTabPage).addToBackStack(null).commitAllowingStateLoss();
-        transaction.add(R.id.id_content,noticeTabPage).addToBackStack(null).commitAllowingStateLoss();
-        transaction.add(R.id.id_content,meTabPage).addToBackStack(null).commitAllowingStateLoss();
-        transaction.commit();
-    }
-
-    public  void  resetImg() {
-        mEarthImageButton.setImageResource(R.drawable.earth);
-        mNearImageButton.setImageResource(R.drawable.near);
-        mNoticeImageButton.setImageResource(R.drawable.notice);
-        mMyImageButton.setImageResource(R.drawable.mine);
-
-    }
-
-    private  void hideFragment(FragmentTransaction transaction) {
-        transaction.hide(earthTabPage);
-        transaction.hide(nearTabPage);
-        transaction.hide(meTabPage);
-        transaction.hide(noticeTabPage);
-
-    }
-
-    private void selectFragment(int i){
-        FragmentTransaction transaction=fm.beginTransaction();
-        hideFragment(transaction);
-
-        switch(i){
+        hideFragments(transaction);
+        switch(id){
             case 0:
-                transaction.show(earthTabPage);
-                mEarthImageButton.setImageResource(R.drawable.earth_clicked);
+                mTabRadioGroup.check(R.id.earth_tab);
+                if(earthFragment==null){
+                    earthFragment=new EarthFragment();
+                    transaction.add(R.id.fragment_container,earthFragment).commitAllowingStateLoss();
+                }else{
+                    transaction.show(earthFragment);
+                }
                 break;
             case 1:
-                transaction.show(nearTabPage);
-                mEarthImageButton.setImageResource(R.drawable.near_clicked);
+                mTabRadioGroup.check(R.id.near_tab);
+                if(nearFragment==null){
+                    nearFragment=new NearFragment();
+                    transaction.add(R.id.fragment_container,nearFragment).commitAllowingStateLoss();
+                }else{
+                    transaction.show(nearFragment);
+                }
                 break;
             case 2:
-                transaction.show(noticeTabPage);
-                mNoticeImageButton.setImageResource(R.drawable.notice_clicked);
+                mTabRadioGroup.check(R.id.notice_tab);
+                if(noticeFragment==null){
+                    noticeFragment=new NoticeFragment();
+                    transaction.add(R.id.fragment_container,noticeFragment).commitAllowingStateLoss();
+                }else{
+                    transaction.show(noticeFragment);
+                }
                 break;
             case 3:
-                transaction.show(meTabPage);
-                mMyImageButton.setImageResource(R.drawable.my_clicked);
-            default:
+                mTabRadioGroup.check(R.id.my_tab);
+                if(meFragment==null){
+                    meFragment=new MeFragment();
+                    transaction.add(R.id.fragment_container,meFragment).commitAllowingStateLoss();
+                }else{
+                    transaction.show(meFragment);
+                }
                 break;
-
         }
-        transaction.commit();
+    }
+
+    private void hideFragments(FragmentTransaction transaction) {
+        if (earthFragment != null) {
+            transaction.hide(earthFragment);
+        }
+        if (nearFragment != null) {
+            transaction.hide(nearFragment);
+        }
+        if (noticeFragment != null) {
+            transaction.hide(noticeFragment);
+        }
+        if (meFragment != null) {
+            transaction.hide(noticeFragment);
+        }
 
     }
 
 
-    @Override
-    public void onClick(View view) {
-        resetImg();
-        switch (view.getId()){
-            case R.id.id_tabButton_earth:
-                selectFragment(0);
-                break;
-            case R.id.id_tabButton_near:
-                selectFragment(2);
-                break;
-            case R.id.id_tabButton_notice:
-                selectFragment(2);
-                break;
-            case R.id.id_tabButton_my:
-                selectFragment(3);
-                break;
-            default:
-                break;
-        }
-    }
 }
