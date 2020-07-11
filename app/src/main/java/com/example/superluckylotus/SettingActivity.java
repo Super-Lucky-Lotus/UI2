6b0225ca1549a8e0f4ee934025534a0f9134efa8
蛋蛋 <for.s@qq.com>
2020-07-12 01:32:34 +0800

    我的界面跳转到设置界面以及返回

--- app/src/main/java/com/example/superluckylotus/SettingActivity.java
+++ app/src/main/java/com/example/superluckylotus/SettingActivity.java
@@ -2,13 +2,39 @@ package com.example.superluckylotus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SettingActivity extends AppCompatActivity {
+
+    ImageButton mBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        mBackBtn=(ImageButton)findViewById(R.id.back);
+        setListeners();
     }
-}
+
+    private void setListeners(){
+        SettingActivity.OnClick onClick = new SettingActivity.OnClick();
+        mBackBtn.setOnClickListener(onClick);
+    }
+
+    private class OnClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v){
+            Intent intent = null;
+            switch (v.getId()){
+                case R.id.back:
+                    intent = new Intent(SettingActivity.this,MainActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+}}
