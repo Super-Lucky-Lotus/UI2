124fc91d8617cc2802a65ccc0b2c77655c14b607
蛋蛋 <for.s@qq.com>
2020-07-18 17:13:03 +0800

    增加了视频滑动

--- app/src/main/java/com/example/superluckylotus/EarthFragment.java
+++ app/src/main/java/com/example/superluckylotus/EarthFragment.java
@@ -1,15 +1,23 @@
 package com.example.superluckylotus;
 
+import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 /**
@@ -47,6 +55,10 @@ public class EarthFragment extends Fragment {
     MoreDialog md;
     CommentDialog cd;
 
+    private RecyclerView recyclerView;
+
+    private DouYinLayoutManager douYinLayoutManager;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_earth,null);
         turnSearchPage_btn = (Button)view.findViewById(R.id.turnSearchPage_btn);
@@ -56,10 +68,139 @@ public class EarthFragment extends Fragment {
         cd=new CommentDialog(getActivity());
         OnClick onclick=new OnClick();
         turnSearchPage_btn.setOnClickListener(onclick);
-        mMore.setOnClickListener(onclick);
-        mComment.setOnClickListener(onclick);
+        //mMore.setOnClickListener(onclick);
+        //mComment.setOnClickListener(onclick);
+        recyclerView = view.findViewById(R.id.recyclerView_dy);
+        initView();
         return view;
     }
+
+    private void initView() {
+        douYinLayoutManager = new DouYinLayoutManager(getActivity(), OrientationHelper.VERTICAL,false);
+        recyclerView.setLayoutManager(douYinLayoutManager);
+        recyclerView.setAdapter(new MyAdapter());
+
+        douYinLayoutManager.setOnViewPagerListener(new OnViewPagerListener() {
+            @Override
+            public void onPageRelease(boolean isNest, View position) {
+                releaseVideo(position);
+
+            }
+
+            @Override
+            public void onPageSelected(boolean isButten, View position) {
+                int index = 0;
+                if (isButten){
+                    index = 0;
+                }else {
+                    index = 1;
+                }
+                playVideo(position);
+
+            }
+        });
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+        //添加封面
+        private int[] imgs = {R.mipmap.img_video_1,R.mipmap.img_video_2,R.mipmap.img_video_1,R.mipmap.img_video_2,R.mipmap.img_video_1,R.mipmap.img_video_2};
+        //添加视频
+        private int[] videos = {R.raw.video_1,R.raw.video_2,R.raw.video_1,R.raw.video_2,R.raw.video_1,R.raw.video_2};
+        public MyAdapter(){
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_video,parent,false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.img_thumb.setImageResource(imgs[position]);
+            holder.videoView.setVideoURI(Uri.parse("android.resource://"+getActivity().getPackageName()+"/"+ videos[position]));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 6;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            ImageView img_thumb;
+            VideoView videoView;
+            ImageView img_play;
+            RelativeLayout rootView;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                img_thumb = itemView.findViewById(R.id.img_thumb);
+                videoView = itemView.findViewById(R.id.video_view);
+                img_play = itemView.findViewById(R.id.img_play);
+                rootView = itemView.findViewById(R.id.root_view);
+            }
+        }
+    }
+
+
+    /**
+     * 停止播放
+     * @param itemView
+     */
+    private void releaseVideo(View  itemView){
+        final VideoView videoView = itemView.findViewById(R.id.video_view);
+        final ImageView imgThumb = itemView.findViewById(R.id.img_thumb);
+        final ImageView imgPlay = itemView.findViewById(R.id.img_play);
+        videoView.stopPlayback();
+        imgThumb.animate().alpha(1).start();
+        imgPlay.animate().alpha(0f).start();
+    }
+
+    /**
+     * 开始播放
+     * @param itemView
+     */
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void playVideo(View itemView) {
+        final VideoView videoView = itemView.findViewById(R.id.video_view);
+        final ImageView imgPlay = itemView.findViewById(R.id.img_play);
+        final ImageView imgThumb = itemView.findViewById(R.id.img_thumb);
+        final RelativeLayout rootView = itemView.findViewById(R.id.root_view);
+        final MediaPlayer[] mediaPlayer = new MediaPlayer[1];
+        videoView.start();
+        videoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                mediaPlayer[0] = mp;
+                mp.setLooping(true);
+                imgThumb.animate().alpha(0).setDuration(200).start();
+                return false;
+            }
+        });
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+            }
+        });
+
+
+        imgPlay.setOnClickListener(new View.OnClickListener() {
+            boolean isPlaying = true;
+            @Override
+            public void onClick(View v) {
+                if (videoView.isPlaying()){
+                    imgPlay.animate().alpha(1f).start();
+                    videoView.pause();
+                    isPlaying = false;
+                }else {
+                    imgPlay.animate().alpha(0f).start();
+                    videoView.start();
+                    isPlaying = true;
+                }
+            }
+        });
+    }
+
 
     private class OnClick implements View.OnClickListener{
         @Override
@@ -70,12 +211,12 @@ public class EarthFragment extends Fragment {
                     intent.setClass(getActivity(),SearchActivity.class);
                     getActivity().startActivity(intent);
                     break;
-                case R.id.more:
+                /*case R.id.more:
                     md.popupWindowDialog( v);
                     break;
                 case R.id.comment:
                     cd.popupWindowDialog( v);
-                    break;
+                    break;*/
             }
         }
     }
